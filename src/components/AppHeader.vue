1b8f404c1a49eb0f33a53c1a683537b484aea94c
claudiopatti <claudiopatti1997@gmail.com>
2024-11-26 12:13:51 +0100

    aggiunto logo e tasti a destra per appartamenti e login

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,31 +1,43 @@
 <script>
 export default {
-  data() {
-    return { 
-      message: 'Template Vite + Vue'
-    }
-  }
+  	data() {
+    	return { 
+      
+    	}
+  	}
 }
 </script>
 
 <template>
-  <header>
-    <h1 class="mb-4">
-      {{ message }}
-    </h1>
-  </header>
+  	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col d-flex justify-content-start">
+					<h1 class="text-black  mb-0">BoolBnb</h1>
+				</div>
+				<div class="col d-flex justify-content-end align-items-center ">
+					<div>
+						<a href="" class="btn btn-success me-4">
+							Appartamenti
+						</a>
+					</div>
+					<div>
+						<a href="" class="btn btn-warning">
+							Login
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+  	</header>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
 header {
-  background-color: $mainBgColor;
-  text-align: center;
-  padding: 20px 0;
-}
-
-h1 {
-  color: $mainColor;
+  	background-color: lightblue;
+  	text-align: center;
+  	padding: 20px 0;
 }
 </style>
