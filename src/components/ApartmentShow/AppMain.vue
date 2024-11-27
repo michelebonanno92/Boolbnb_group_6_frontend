756c0eec4bd130b386ab92548010161009d31996
ChristopherS24 <chryskies@gmail.com>
2024-11-27 12:42:45 +0100

    applicato stile main di show apartment

--- src/components/ApartmentShow/AppMain.vue
+++ src/components/ApartmentShow/AppMain.vue
@@ -11,18 +11,32 @@ export default {
 <template>
   <main>
 
-    <div class="container">
+    <div class="container my-5" style="width: 1000px;">
 
       <div class="row">
         
         <div class="container text-center">
-          <h1>
-            appartamento
-          </h1>
+          <div class="card mb-3">
+            <img src="https://www.bnbinrome.com/images/261428888.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Appartamento</h5>
+              <p class="card-text">Breve descrizione</p>
+              <p class="card-text"><small class="text-body-secondary">servizi</small></p>
+           </div>
+          </div>
         </div>
 
       </div>
+
       
+    </div>
+    <div class="container my-5">
+      <div class="d-flex flex-row">
+        <button type="button" class="btn btn-secondary btn-lg">Torna indietro</button>
+      </div>
+      <div class="d-flex flex-row-reverse">
+        <button type="button" class="btn btn-primary btn-lg">Invia email</button>
+      </div>
     </div>
     
   </main>
@@ -32,13 +46,5 @@ export default {
 <style lang="scss" scoped>
 @use '../../assets/scss/partials/variables' as *;
 
-main {
-  text-align: center;
-  padding: 20px 0;
-}
 
-
-h1 {
-  color: black;
-}
 </style>
