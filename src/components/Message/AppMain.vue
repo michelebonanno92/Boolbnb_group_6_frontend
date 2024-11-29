594656ba2f79b302b18242a8b394bc55569a5f20
ChristopherS24 <chryskies@gmail.com>
2024-11-29 13:12:20 +0100

    implementati collegamenti pag. message attrraverso slug

--- src/components/Message/AppMain.vue
+++ src/components/Message/AppMain.vue
@@ -1,126 +1,43 @@
 <script>
-// import axios from 'axios';
-// export default {
-//   data() {
-//     return { 
-//       searchQuery: '',
-//       suggestions: [],
-//       apiKey: 'KtAYjlAUfMLakTMNV7iootfwwERDicp1', // Inserisci la tua API Key qui
-//       apartments: [],
-//       filteredApartments: [],
-//     }
-//   },
-//   mounted() {
-//     this.getApartments();
-//     },
-//   methods: {
-//     // chiamata api apartments
-//     getApartments() {
-//         axios
-//           .get('http://127.0.0.1:8000/api/apartments')
-//           .then((res) => {
-//             // console.log(res.data.apartments);
+import axios from 'axios';
 
-//             this.apartments = res.data.apartments;
-//             // console.log(this.apartments);
-           
-//           });
-//       },
-
-//       // suggerimento ricerca
-//       fetchSuggestions() {
-//       if (this.searchQuery.length < 1) {
-//         this.suggestions = [];
-//         return;
-//       }
-
-//       const url = `https://api.tomtom.com/search/2/geocode/${encodeURIComponent(
-//         this.searchQuery
-//       )}.json`;
-
-//       axios
-//         .get(url, {
-//           params: {
-//             key: this.apiKey,
-//             limit: 5,
-//             language: "it-IT",
-//             // countrySet: "IT",
-//           },
-//         })
-//         .then((response) => {
-//           this.suggestions = response.data.results;
-//         })
-//         .catch((error) => {
-//           console.error("Errore nella ricerca:", error);
-//         });
-//     },
-//     selectSuggestion(suggestion) {
-//       const latitude = suggestion.position.lat;
-//       const longitude = suggestion.position.lon;
-//       const address = suggestion.address.freeformAddress;
-
-      
-//       // Reindirizza alla pagina `search-results` con i dati passati come query
-      
-//       // Filtra gli appartamenti entro 10 km
-//       this.filteredApartments = this.apartments
-//       .map((apartment) => {
-//           const distance = this.calculateDistance(
-//             latitude,
-//             longitude,
-//             apartment.latitude,
-//             apartment.longitude,
-//           );
-//           return { ...apartment, distance }; // Aggiunge la distanza all'appartamento
-//         })
-//         .filter((apartment) => apartment.distance <= 50); // Filtra per raggio
-        
-
-//         this.$router.push({
-//           name: 'apartments',
-//           query: {
-//             lat: latitude,
-//             lon: longitude,
-//             address: address,
-//           },
-//         });
-//         // Aggiorna la barra di ricerca con il risultato selezionato
-//       this.searchQuery = suggestion.address.freeformAddress;
-//       this.suggestions = [];
-//     },
-//     calculateDistance(lat1, lon1, lat2, lon2) {
-//       const R = 6371; // Raggio della Terra in km
-//       const dLat = this.degToRad(lat2 - lat1);
-//       const dLon = this.degToRad(lon2 - lon1);
-//       const a =
-//         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-//         Math.cos(this.degToRad(lat1)) *
-//           Math.cos(this.degToRad(lat2)) *
-//           Math.sin(dLon / 2) *
-//           Math.sin(dLon / 2);
-//       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-//       return R * c; // Distanza in km
-//     },
-//     degToRad(deg) {
-//       return deg * (Math.PI / 180);
-//     },
-
-    
-//   },
-// }
 export default {
   data() {
     return { 
-      message: 'Main'
+      message: 'Main',
+	  	slug: this.$route.query.slug,
+
     }
   },
+  mounted() {
+		this.getApartments();
 
- computed: {
-  selectedAppartamentoSlug() {
-    return this.$route.query.slug;
+		
+  },
+  computed: {
+ selectedAppartamentoSlug() {
+   return this.$route.query.slug;
+ }
+},
+  methods: {
+    getApartments() {
+		axios
+		  .get('http://127.0.0.1:8000/api/apartments' + '/' + this.slug)
+		  .then((res) => {
+			console.log(res.data.apartment);
+			console.log(res.data.apartment.slug);
+
+	
+			this.apartment = res.data.apartment;
+			// console.log(this.apartments);
+    });
+		},
   }
 }
-}
+
+
+    
+
 </script>
 
 <template>
@@ -144,7 +61,8 @@ export default {
     <div class="container my-5">
       <!-- deve tornare indietro nell'appartamento giusto -->
       <div class="d-flex flex-row">
-        <router-link :to="{ name: 'apartment-show', params: { slug: selectedAppartamentoSlug } }" class="btn btn-secondary btn-lg">Torna indietro</router-link> 
+        <!-- <router-link :to="{ name: 'apartment-show', params: { slug: selectedAppartamentoSlug } }" class="btn btn-secondary btn-lg">Torna indietro</router-link>  -->
+         <router-link :to="{ name: 'apartment-show', params: { slug: selectedAppartamentoSlug } }"  class="btn btn-secondary btn-lg">Torna indietro</router-link>
       </div>
       
       <div class="d-flex flex-row-reverse">
@@ -152,73 +70,6 @@ export default {
       </div>
     </div>
 
-    <!-- <div class="container search-container mb-4">
-		<input
-		v-model="searchQuery"
-		@input="fetchSuggestions"
-		placeholder="Cerca un indirizzo..."
-		class="search-bar"
-		/>
-		<ul v-if="suggestions.length" class="suggestions-list text-start">
-			<li 
-				v-for="(suggestion, index) in suggestions"
-				:key="index"
-				@click="selectSuggestion(suggestion)"
-			>
-				{{ suggestion.address.freeformAddress }}
-			</li>
-		</ul>
-
-		<div v-if="filteredApartments.length">
-		<h3>Appartamenti trovati entro 10 km:</h3>
-			<div class="container">
-				<div class="row">
-					
-					<div v-for="apartment in filteredApartments" :key="apartment.id" class="col-12 col-sm-6 col-md-6 col-lg-4 mb-3">
-						
-						<div>
-							<div class="card my-card p-3">
-								<div class="text-center">
-									<img :src=" apartment.full_image_url " class="card-img-top img-fluid" :alt=" apartment.title ">
-								</div>
-								<h4 class="mb-2">
-									{{ apartment.title }} ({{ apartment.distance.toFixed(2) }} km)
-								</h4>
-								<ul class="text-start h-100">
-									<li>
-										Stanze: {{ apartment.rooms }}
-									</li>
-									<li>
-										Letti: {{ apartment.beds }}
-									</li>
-									<li>
-										Bagni: {{ apartment.toilets }}
-									</li>
-								</ul>
-								<div  class="mt-4 h-100">
-									<ul class="service-list">
-										<li>
-											placeholder servizi
-										</li>
-									</ul>
-								</div>
-								<div>
-									<router-link :to="{ name: 'apartment-show' , params: { slug: apartment.slug }}" class="btn btn-outline-success w-100">Dettagli</router-link>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		
-		</div>
-		<div v-else-if="!filteredApartments.length && !searchQuery == '' ">
-			
-			<h3>Nessun Appartamento trovato</h3>
-
-		</div>
-  </div>
-     -->
   </main>
 
 </template>
