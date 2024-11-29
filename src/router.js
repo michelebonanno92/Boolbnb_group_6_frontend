f42daefb05622167ab803706d013e4e5615b9a8d
ChristopherS24 <chryskies@gmail.com>
2024-11-29 11:01:37 +0100

    aggiunta pagina messaggio

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import HomePage from './views/HomePage.vue';
 import ApartmentShow from './views/ApartmentShow.vue';
 import Apartments from './views/Apartments.vue';
 import NotFoundPage from './views/NotFoundPage.vue';
+import Message from './views/Message.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -27,6 +28,11 @@ const router = createRouter({
             path: '/:pathMatch(.*)*',
             name: 'not-found',
             component: NotFoundPage
+        },
+        {
+            path: '/messaggio',
+            name: 'message',
+            component: Message
         },
     ]
 });
